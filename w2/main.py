1f13379c3c3ffd5f1d338ed17e59501db7ec5c55
Maria Hernandez <elijah5409@gmail.com>
2022-01-19 17:54:58 -0800

    starting setup complete

---
+++ w2/main.py
@@ -0,0 +1,13 @@
+
+print("Welcome to the band name generator.")
+high_school = input("Which high school did you attend?")
+print(high_school)
+school = input("What was your high school mascot?")
+print(school)
+print("Your band name could be " + high_school + school)
+
+
+print(type(two_digit_number))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+result = int(first_digit)+int(second_digit)
